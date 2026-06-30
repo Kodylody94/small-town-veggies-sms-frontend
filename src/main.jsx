c156441d7a0e1337136bb687821b722d321b89e8
Kodylody94 <119109316+Kodylody94@users.noreply.github.com>
2026-06-29 23:22:56 -0500

    Guard application startup and rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import AppErrorBoundary from './components/AppErrorBoundary.jsx';
+import './index.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('The application root element was not found.');
+}
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppErrorBoundary>
   </StrictMode>,
 );
